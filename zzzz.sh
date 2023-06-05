b9b4227b363eef1a4b761394f9aa18e67c25bf0d
fuming <fuming.h@igg.com>
2023-06-05 13:17:07 -0700

    no message

--- zzzz.sh
+++ zzzz.sh
@@ -1,7 +1,8 @@
-#!/bin/bash                                                                                                                                                                                                        
-SU='"The Crusade of Edda" event starting soon!'                                                                                                                                                                    
-BODY=`cat mail1.txt`                                                                                                                                                                                               
-SID=1                                                                                                                                                                                                              
-NB=${BODY/\"SID\"/$SID}                                                                                                                                                                                            
-echo $NB                                                                                                                                                                                                           
-/bin/curl -d $NB http://10.148.74.248:7350/v1/igg/mail
+#!/bin/bash
+SU='"The Crusade of Edda" event starting soon!'
+BODY=`cat mail1.txt`
+SID=11
+NB=${BODY/\"SID\"/$SID}
+echo $NB > nb.json
+sleep 5
+/usr/bin/curl -d@nb.json http://10.128.58.111:7350/v1/igg/mail
