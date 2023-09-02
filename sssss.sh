15d4f726e31df8a5d24e548e1bde2ef739b75b78
fuming <fuming.h@igg.com>
2023-09-01 21:18:24 -0700

    no message

--- sssss.sh
+++ sssss.sh
@@ -2,4 +2,6 @@
 while IFS=$'\t' read -r uuid zone; do
     echo "UUID: $uuid" "Zone: $zone"
     redis-cli  -h ca001-central-1.bdct0f.ng.0001.usw2.cache.amazonaws.com -p 6379 zscore $zone $uuid
+	redis-cli  -h ca001-central-1.bdct0f.ng.0001.usw2.cache.amazonaws.com -p 6379 zadd  $zone 29760306401831 $uuid
+	redis-cli  -h ca001-central-1.bdct0f.ng.0001.usw2.cache.amazonaws.com -p 6379 zscore $zone $uuid
 done < soulhall.txt
