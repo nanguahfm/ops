3c2cdb503834b0b81761d4ea993e75c17a934092
fuming <fuming.h@igg.com>
2024-03-11 15:48:46 -0700

    no message

--- minescore.sh
+++ minescore.sh
@@ -20,11 +20,26 @@ NOW=`date '+%s'`
 #SQL="select array_to_json(array_agg(row_to_json(t))) as array_to_json from (select name from server_list) t"
 SQL="select score, uid from top_record where id = 'GLOBAL_MINE_14' and uid = '$1' "
 #SQL="select  concat(min(t.name), 'SSS', t.serverid)  from (select s."name",  c.serverid  from server_list  s, cluster_ids c where s.cluster = c."cluster" order by c.serverid) as  t  group by t.serverid"
-echo $SQL 2222222222222222222
+echo $SQL 
 #INFO=`/usr/bin/psql -h ${HOST} -p ${PORT} -U ${USER} -d ${DATABASE} -A -t -c "${SQL}"|jq '.[]|.name'|sed 's/"//g'`
-
+echo "----------------------------"
 INFO=`/usr/bin/psql -h ${HOST} -p ${PORT} -U ${USER} -d ${DATABASE} -A -t -c "${SQL}"`
 echo $INFO
+redis-cli -h ca001-central-1.bdct0f.ng.0001.usw2.cache.amazonaws.com -p 6379 zscore GLOBAL_MINE_14 $1
+echo "----------------------------modify-----------------"
+((ss = $2 * 10000000 + 948707))
+echo $ss
+SQL2='UPDATE public.top_record SET maxscore=$2 WHERE id='GLOBAL_MINE_14' AND uid='$1'::uuid::uuid;'
+INFO2=`/usr/bin/psql -h ${HOST} -p ${PORT} -U ${USER} -d ${DATABASE} -A -t -c "${SQL}"`
+
+redis-cli -h ca001-central-1.bdct0f.ng.0001.usw2.cache.amazonaws.com -p 6379 zadd GLOBAL_MINE_14 $1  $ss
+echo "----------------------------"
+INFO=`/usr/bin/psql -h ${HOST} -p ${PORT} -U ${USER} -d ${DATABASE} -A -t -c "${SQL}"`
+echo $INFO
+redis-cli -h ca001-central-1.bdct0f.ng.0001.usw2.cache.amazonaws.com -p 6379 zscore GLOBAL_MINE_14 $1
+
+echo "----------------------------"
+
 #exit
 if [ "$1" = "" ];then
 	echo "need uid"
