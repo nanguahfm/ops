31f9f5e3e32a368b70cadfa9a7015a9be874e1b1
fuming <fuming.h@igg.com>
2024-04-27 14:18:11 -0700

    no message

--- acl.sh
+++ acl.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-while true; do
-  now=$(date '+%Y-%m-%d %H:%M:%S')
-  echo "Current time: $now"
-  redis-cli -h ca001-central-1.bdct0f.ng.0001.usw2.cache.amazonaws.com  -p 6379 del ACL_UNIQ_ACL_TOTAL_UNIQ_FRIEND
-  sleep 60
-done
+git clone https://github.com/nanguahfm/game.git /patch/tool
